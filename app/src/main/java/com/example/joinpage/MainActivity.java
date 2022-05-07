d4753181ca62122319f6021f0a439e83005db7ac
Zandevier <ninaddeshmukh7532@gmail.com>
2022-05-08 00:28:41 +0530

    added bg

--- app/src/main/java/com/example/joinpage/MainActivity.java
+++ app/src/main/java/com/example/joinpage/MainActivity.java
@@ -3,18 +3,26 @@ package com.example.joinpage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public Button button;
+    public TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getActionBar().hide();
+        text = (TextView) findViewById(R.id.text);
+        text.setEllipsize(TextUtils.TruncateAt.MARQUEE);
+        text.setSelected(true);
+
 
         button = (Button) findViewById(R.id.btn1);
         button.setOnClickListener(new View.OnClickListener() {
