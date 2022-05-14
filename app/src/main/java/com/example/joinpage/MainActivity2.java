0bcba21a791a6258179fe163b7ab8ed6b338deef
Zandevier <ninaddeshmukh7532@gmail.com>
2022-05-14 11:57:39 +0530

    changes added

--- app/src/main/java/com/example/joinpage/MainActivity2.java
+++ app/src/main/java/com/example/joinpage/MainActivity2.java
@@ -3,12 +3,15 @@ package com.example.joinpage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-    public TextView textView;
+    public TextView textView,textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,13 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         textView =(TextView) findViewById(R.id.signup);
+        textView2 = (TextView) findViewById(R.id.tt2);
+
+        textView2.setPaintFlags(textView2.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
+        textView.setPaintFlags(textView.getPaintFlags()|Paint.UNDERLINE_TEXT_FLAG);
+//        LayerDrawable bottomBorder = getBorders(Color.BLACK,0,0,0,10);
+//        textView2.setBackground(bottomBorder);
+
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
