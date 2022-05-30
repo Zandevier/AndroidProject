87df34c6ebf8356671e0787b4ff35c7a0424c0e0
Zandevier <ninaddeshmukh7532@gmail.com>
2022-05-30 16:45:19 +0530

    changes added

--- app/src/main/java/com/example/joinpage/MainActivity2.java
+++ app/src/main/java/com/example/joinpage/MainActivity2.java
@@ -8,11 +8,12 @@ import android.graphics.Paint;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
     public TextView textView,textView2;
-
+    public Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +21,7 @@ public class MainActivity2 extends AppCompatActivity {
 
         textView =(TextView) findViewById(R.id.signup);
         textView2 = (TextView) findViewById(R.id.tt2);
+        login = (Button) findViewById(R.id.login);
 
         textView2.setPaintFlags(textView2.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
         textView.setPaintFlags(textView.getPaintFlags()|Paint.UNDERLINE_TEXT_FLAG);
@@ -35,5 +37,12 @@ public class MainActivity2 extends AppCompatActivity {
             }
         });
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ns_page = new Intent(MainActivity2.this,MainActivity4.class);
+                startActivity(ns_page);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/joinpage/MainActivity4.java
+++ app/src/main/java/com/example/joinpage/MainActivity4.java
@@ -2,13 +2,46 @@ package com.example.joinpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity4 extends AppCompatActivity {
+
+    public ImageView imageView2,imageView3,imageView4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+        imageView3 = (ImageView) findViewById(R.id.imageView3);
+        imageView4 = (ImageView) findViewById(R.id.imageView4);
+
+
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent chrome_link  = new Intent(Intent.ACTION_VIEW, Uri.parse("https://results.eci.gov.in/"));
+                startActivity(chrome_link);
+            }
+        });
+        imageView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent chrome_link  = new Intent(Intent.ACTION_VIEW, Uri.parse("https://results.eci.gov.in/ResultPcByeApril2022/partywiseresult-S25.htm"));
+                startActivity(chrome_link);
+            }
+        });
+        imageView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent chrome_link  = new Intent(Intent.ACTION_VIEW, Uri.parse("https://ceo.maharashtra.gov.in/"));
+                startActivity(chrome_link);
+            }
+        });
     }
 }
